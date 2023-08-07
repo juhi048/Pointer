4abdcee9ee2b827f7816ea45f7a0b47448092572
Juhi <juhirastogi048@gmail.com>
2023-08-07 18:01:08 +0530

    Pointer

--- pointer_implementation.cpp
+++ pointer_implementation.cpp
@@ -4,10 +4,6 @@ int main(){
     int a=10;
     int *aptr=&a;
 
-    char b = 'y';
-    char *bptr=&b;
-
-
     cout<<"The value of an integer type"<<endl;
     cout<<a<<endl;
     cout<<aptr<<endl;
@@ -16,13 +12,7 @@ int main(){
     aptr++;
     cout<<*aptr<<endl; // 4 will be added in the address value because of the 4 bytes size.
     
-    cout<<"The value of a character type"<<endl;
-    cout<<b<<endl;
-    cout<<bptr<<endl;
-    cout<<*bptr<<endl;
-
-    bptr++;
-    cout<<bptr<<endl; // 1 will be added in the address value because of the 1 bytes size.
+    // in case of the character array pointer 1 will be added in the address value because of the 1 bytes size.
 
     return 0;
 }
